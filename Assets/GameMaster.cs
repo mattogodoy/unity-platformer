cb7ff0e4ec1d8ca293f57260bae44aca3e9883d9
Matias Godoy <matiasalejandro.godoy@lbigroup.es>
2015-12-16 15:50:37 +0100

    temp: Finished tutorial video number 12.

---
+++ Assets/GameMaster.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameMaster : MonoBehaviour {
+
+	public static GameMaster gm;
+
+	void Start () {
+		if(gm == null) {
+			gm = GameObject.FindGameObjectWithTag ("GM").GetComponent<GameMaster>();
+		}
+	}
+
+	public Transform playerPrefab;
+	public Transform spawnPoint;
+	public int spawnDelay = 2;
+
+	public IEnumerator RespawnPlayer () {
+		Debug.Log ("TODO: Add spawn sound.");
+		yield return new WaitForSeconds(spawnDelay);
+		Instantiate (playerPrefab, spawnPoint.position, spawnPoint.rotation);
+		Debug.Log ("TODO: Add spawn particles.");
+	}
+
+	public static void KillPlayer (Player player) {
+		Debug.Log ("Player is dead!");
+		Destroy (player.gameObject);
+		gm.StartCoroutine(gm.RespawnPlayer ());
+	}
+}
--- Assets/Weapon.cs
+++ Assets/Weapon.cs
@@ -6,10 +6,12 @@ public class Weapon : MonoBehaviour {
 	public float fireRate = 0;
 	public float damage = 10;
 	public LayerMask whatToHit;
-
+	public Transform bulletTrailPrefab;
+	public Transform muzzleFlashPrefab;
+	float timeToSpawn = 0;
+	public float spawnRate = 10;
 	float timeToFire = 0;
 	Transform firePoint;
-
 
 	// Use this for initialization
 	void Awake () {
@@ -39,11 +41,27 @@ public class Weapon : MonoBehaviour {
 		Vector2 firePointPosition = new Vector2 (firePoint.position.x, firePoint.position.y);
 
 		RaycastHit2D hit = Physics2D.Raycast (firePointPosition, mousePosition - firePointPosition, 100, whatToHit);
+
+		// Limit the amounth of spawned bullet trails
+		if (Time.time >= timeToSpawn) {
+			effect ();
+			timeToSpawn = Time.time + 1 / spawnRate;
+		}
+
 		Debug.DrawLine (firePointPosition, (mousePosition - firePointPosition) * 100, Color.cyan);
 
 		if (hit.collider != null) {
 			Debug.DrawLine (firePointPosition, hit.point, Color.red, 1.0f);
 			Debug.Log("We hit: " + hit.collider.name + " and did: " + damage + " damage.");
 		}
+	}
+
+	void effect () {
+		Instantiate (bulletTrailPrefab, firePoint.position, firePoint.rotation);
+		Transform clone = (Transform)Instantiate (muzzleFlashPrefab, firePoint.position, firePoint.rotation);
+		clone.parent = firePoint;
+		float size = Random.Range (0.4f, 0.9f);
+		clone.localScale = new Vector3 (size, size, 0);
+		Destroy (clone.gameObject, 0.02f);
 	}
 }
