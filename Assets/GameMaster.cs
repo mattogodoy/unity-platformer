b54660b7d83096f1cd3209bb1d66fa4e79428be1
Matias Godoy <matiasalejandro.godoy@lbigroup.es>
2015-12-16 16:28:00 +0100

    temp: Finished tutorial video number 13.

--- Assets/GameMaster.cs
+++ Assets/GameMaster.cs
@@ -14,12 +14,14 @@ public class GameMaster : MonoBehaviour {
 	public Transform playerPrefab;
 	public Transform spawnPoint;
 	public int spawnDelay = 2;
+	public Transform spawnPrefab;
 
 	public IEnumerator RespawnPlayer () {
-		Debug.Log ("TODO: Add spawn sound.");
 		yield return new WaitForSeconds(spawnDelay);
+		GetComponent<AudioSource>().Play ();
 		Instantiate (playerPrefab, spawnPoint.position, spawnPoint.rotation);
-		Debug.Log ("TODO: Add spawn particles.");
+		GameObject clone = Instantiate (spawnPrefab, spawnPoint.position, spawnPoint.rotation) as GameObject;
+		Destroy (clone, 3f);
 	}
 
 	public static void KillPlayer (Player player) {
